48e7160be752cc75bdef76de6ba23bc10d4c8808
Kxmm04 <Arpiwat0204@gmail.com>
2025-08-21 03:43:25 +0700

    Update script.js : 03:43

--- script.js
+++ script.js
@@ -42,8 +42,11 @@ $(document).ready(function () {
         let dateInput = $("#dateInput").val();
 
         // ตรวจสอบวันปัจจุบัน
-        let today = new Date().toLocaleDateString('en-CA');
-
+        let now = new Date();
+        let year = now.getFullYear();
+        let month = String(now.getMonth() + 1).padStart(2, '0');
+        let day = String(now.getDate()).padStart(2, '0');
+        let today = `${year}-${month}-${day}`;
         if (dateInput !== today) {
             Swal.fire({
                 icon: "error",
